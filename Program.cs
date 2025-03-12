5eb022b6e035bae9700c69b5953cb626b2148086
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 07:19:59 +0700

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using modul4_103022300149;
+
+class program {
+    static void Main() {
+        kodeProduk KP = new kodeProduk();
+        Console.WriteLine("Masukkan nama produk: ");
+        string produk = Console.ReadLine();
+
+        string kode = KP.getKodeProduk(produk);
+        if (kode != null)
+        {
+            Console.WriteLine($"Kode Produk {produk}: {kode}");
+        }
+        else {
+            Console.WriteLine("Kode Produk tidak ditemukan");
+        }
+    }
+};
---
+++ kodeProduk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300149
+{
+    class kodeProduk
+    {
+        public String getKodeProduk(string produk) {
+            Dictionary<string, string> kodeProduk = new Dictionary<string, string>
+            {
+                {"Laptop", "E100"},
+                {"Smartphone", "E101"},
+                {"Tablet", "E102"},
+                {"Headset", "E103"},
+                {"Keyboard", "E104"},
+                {"Mouse", "E105"},
+                {"Printer", "E106"},
+                {"Monitor", "E107"},
+                {"Smartwatch", "E108"},
+                {"Kamera", "E109"}
+            };
+            return kodeProduk.ContainsKey(produk) ? kodeProduk[produk] : "Produk atau Kode Produk tidak ditemukan";
+        }
+    }
+}
